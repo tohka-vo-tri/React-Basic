7263da1fc29700d43c6042cf0f61077127b7bebb
tohka-vo-tri <imchibitouka@gmail.com>
2023-09-25 22:48:36 +0700

    video dyamic data templates

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 //jsx
 //bable 
 const App = () => {
+  let name = 'Eric';
+  let number = 2021;
+  let boolean = true;
+  let obj = { name: 'Eric', chanel: 'hoi em hiu' };
+  let link = 'https://desktop.github.com';
   return (
     <div className="App">
+      {console.log('check object:,', obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello word  with reatct</h1>
+        <h1>hello word  with reatct {name} in {number} </h1>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target='_blank'>visit the github </a>
       </header>
     </div>
   );
