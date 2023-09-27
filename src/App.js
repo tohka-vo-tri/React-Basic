96825fcdde21652604613c077fb27f7782331c33
tohka-vo-tri <imchibitouka@gmail.com>
2023-09-27 22:32:55 +0700

    video10 printt list aray

--- src/App.js
+++ src/App.js
@@ -9,11 +9,26 @@ const App = () => {
 
   let [name, setName] = useState('Erik');//[a1, b1 ,c1.....]
   const [addres, setAddres] = useState('');
-  const handleEventClick = (event) => {
-    console.log(addres)
-    setName(addres);
-    //this.setState
-    console.log('>>>click me', name)
+  const [todos, setTodos] = useState([
+    { id: "todo 1", title: "whatching react with em hiu" },
+    { id: "todo 2", title: "co len  em hiu" },
+    { id: "todo 4", title: "sap duoc roi" }
+  ]);
+
+  const handleEventClick = (event) => {// cap nhat lai bien
+    // console.log(addres)
+    // setName(addres);
+    // //this.setState
+    // console.log('>>>click me', name)
+    //hook not merge state
+    //...spread 
+    if (!addres) {
+      alert('empty input') // truong hop k duoc de trong
+      return; // de cho thoat ra kho ham nay
+    }
+    let newtodo = { id: 'abc', title: addres };
+    setTodos([...todos, newtodo])
+    setAddres('');// lam cho thanh input trong
   }
 
   const handleOnChangeInpput = (event) => {
@@ -27,6 +42,17 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>hello word  with reatct {name}  </h1>
+        <div className="todo-container">
+          {todos.map(todos => {
+            console.log('>>>check todo', todos)
+            return (
+              <li className='todo-child' key={todos.id}>{todos.title}</li>
+            )
+          })}
+          <li className="todo-child">em hiu doing homework</li>
+          <li className="todo-child">em hiu doing homework</li>
+
+        </div>
         <input type="text" value={addres} onChange={(event) => handleOnChangeInpput(event)} />
         <button type="button" onClick={(event) => handleEventClick(event)}>Click me </button>
       </header>
