5084c1a70e4fd834ec3911bbcdcefe1b232c33b4
tohka-vo-tri <imchibitouka@gmail.com>
2023-09-27 21:49:01 +0700

    video 8 states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './view/Nav.js'
+import { useState } from 'react';
 //logic + ctemplate 
 //jsx
 //bable 
 const App = () => {
-  let name = 'Eric';
-  let number = 2021;
-  let boolean = true;
-  let obj = { name: 'Eric', chanel: 'hoi em hiu' };
-  let link = 'https://desktop.github.com';
+
+  let [name, setName] = useState('Erik');//[a1, b1 ,c1.....]
+  const [addres, setAddres] = useState('');
+  const handleEventClick = (event) => {
+    console.log(addres)
+    setName(addres);
+    //this.setState
+    console.log('>>>click me', name)
+  }
+
+  const handleOnChangeInpput = (event) => {
+    setAddres(event.target.value)
+    console.log(event.target.value)
+  }
+  // re-render
   return (
     <div className="App">
-      {console.log('check object:,', obj)}
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello word  with reatct {name} in {number} </h1>
-        <p>{JSON.stringify(obj)}</p>
-        <a href={link} target='_blank'>visit the github </a>
+        <h1>hello word  with reatct {name}  </h1>
+        <input type="text" value={addres} onChange={(event) => handleOnChangeInpput(event)} />
+        <button type="button" onClick={(event) => handleEventClick(event)}>Click me </button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
