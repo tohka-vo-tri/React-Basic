4e0e1f121dfdf9b76710fb95a9a3f8edaacab55d
tohka-vo-tri <imchibitouka@gmail.com>
2023-09-27 23:39:23 +0700

    video12 reuse componment

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ const App = () => {
   let [name, setName] = useState('Erik');//[a1, b1 ,c1.....]
   const [addres, setAddres] = useState('');
   const [todos, setTodos] = useState([
-    { id: "todo 1", title: "whatching react with em hiu" },
-    { id: "todo 2", title: "co len  em hiu" },
-    { id: "todo 4", title: "sap duoc roi" }
+    { id: "todo 1", title: "whatching react with em hiu", type: 'eric' },
+    { id: "todo 2", title: "co len  em hiu", type: 'eric' },
+    { id: "todo 3", title: "sap duoc roi", type: 'hoc react' },
+    { id: "todo 4", title: "sap duoc roi", type: 'hoi em hiu' }
   ]);
 
   const handleEventClick = (event) => {// cap nhat lai bien
@@ -27,7 +28,7 @@ const App = () => {
       alert('empty input') // truong hop k duoc de trong
       return; // de cho thoat ra kho ham nay
     }
-    let newtodo = { id: 'abc', title: addres };
+    let newtodo = { id: 'abc', title: addres, type: 'eric' };
     setTodos([...todos, newtodo])
     setAddres('');// lam cho thanh input trong
   }
@@ -39,13 +40,18 @@ const App = () => {
   // re-render
   return (
     <div className="App">
-      <Nav />
+
       <header className="App-header">
+        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>hello word  with reatct {name}  </h1>
         <Todo
-          mydata={todos}//bên trái tên của props muốn truyền đi bên phải là giá trị của nó
+          todos={todos}//bên trái tên của props muốn truyền đi bên phải là giá trị của nó
           title='all pro'
+        />
+        <Todo
+          todos={todos.filter(item => item.type === 'eric')}//fillter loc cac phan tu co type la eric
+          title={'Erics todo'}
         />
         <input type="text" value={addres} onChange={(event) => handleOnChangeInpput(event)} />
         <button type="button" onClick={(event) => handleEventClick(event)}>Click me </button>
--- src/view/Todo.js
+++ src/view/Todo.js
@@ -1,16 +1,20 @@
 const todo = (props) => {
     //properties
     //chuyển dữ liệu từ paretn  xuống child
-    console.log('>>check my prop', props);
-    const todos = props.mydata;
+    // console.log('>>check my prop', props);
+    const todos = props.todos;
     return (
         <div className="todo-container">
+            <div className="title">
+                {props.title}
+            </div>
             {todos.map(todos => {
                 console.log('>>>check todo', todos)
                 return (
                     <li className='todo-child' key={todos.id}>{todos.title}</li>
                 )
             })}
+            <hr></hr>
         </div>
     )
 }
